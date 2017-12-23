590c26080ece91d81b8787cfce96abed0a11abe4
Joanna Purosto <joanna.purosto@gmail.com>
2017-12-23 19:57:17 +0200

    small adjusments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# practice
+# Practice
 Metropolia course | Developing Modern Web Platforms
 
 This is my first actual website project and I have been testing foundation and playing around w JavaScript.
--- styles/js/app.js
+++ styles/js/app.js
@@ -1,13 +1,10 @@
-// $(document).foundation()
+// PROJECT PAGE
 
-//Scroll event to project page
-
-
+//Scroll event
 document.onscroll = function() {
   var floatingBox = document.getElementById('scroll')
   floatingBox.className = "scroll-visible";
 }
-
 
 //Change project titles
 var bangs = document.getElementById("bangs");
@@ -16,12 +13,3 @@ bangs.addEventListener("mouseover", changeText, true);
 function changeText() {
   bangs.innerHTML = 'Yes, I created a website for them';
 }
-
-
-
-//haluaisin vielä muuttaa niin, että kun hiiri poistuu, teksti muuttuu takaisin
-//var el = document.getElementById("bangs");
-//el.addEventListener("mouseoff", showData, true);
-
-//function showData() {
-//  document.getElementById("bangs").innerHTML = 'Website for Studio Bangs';}
